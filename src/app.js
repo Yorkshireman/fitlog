118d38027b8777439d90ab12c4d41f51b187deff
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-06 00:46:39 +0000

    add /blog route and check json mock can be sent

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+      posts = require('./mock/posts.json');
 
 var app = express();
 
 app.get('/', function(req, res){
   res.send("I Love You!");
+});
+
+app.get('/blog', function(req, res){
+  res.send(posts);
 });
 
 app.listen(3000, function(){
