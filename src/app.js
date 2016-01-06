fbe3ed43b91dc114fad185acd8ef850bd28c79f1
Andrew Stelmach <andrew_stelmach@hotmail.com>
2016-01-06 00:40:40 +0000

    create mock json

--- src/app.js
+++ src/app.js
@@ -4,8 +4,10 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(request, response){
-  response.send("I Love You!");
+app.get('/', function(req, res){
+  res.send("I Love You!");
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("The frontend server is running on port 3000");
+});
